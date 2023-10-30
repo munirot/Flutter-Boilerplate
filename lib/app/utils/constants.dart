dcef29aa2776b6ae803eb8c55120d541e2e448ae
Munirot Thon <51738781+munirot@users.noreply.github.com>
2023-10-30 21:52:50 +0900

    Update constants.dart
    remove key

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const THE_MOVIE_DB_API_KEY = "844c79598fe5f6ddea789cfa2a57c4df";
-  static const THE_MOVIE_DB_IMG_PATH = "https://image.tmdb.org/t/p/original"; 
+  static const THE_MOVIE_DB_API_KEY = "";
+  static const THE_MOVIE_DB_IMG_PATH = ""; 
 }
